5921f7de040fcd04b7603a4393aa3592be502d1a
Daviti Metreveli <44302299+SNYPl@users.noreply.github.com>
2021-04-16 02:16:16 +0400

    Add files via upload

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -2,6 +2,8 @@ let colorChange = document.querySelectorAll(".colorChange");
 let menuList = document.querySelectorAll("#click-menu");
 let mainText = document.querySelectorAll(".main-text-menu");
 
+let transactionList = document.querySelector(".transactions");
+
 let showMenu = function () {
   for (let i = 0; i < mainText.length; i++) {
     mainText[i].addEventListener("click", function (event) {
@@ -31,3 +33,70 @@ let colChange = function () {
 
 showMenu();
 colChange();
+
+let data = [{
+    id: "#SK2540",
+    billingName: "Neal Matthews",
+    date: "07 Oct, 2019",
+    total: "$400",
+    status: "Paid",
+    paymentMethod: ["Mastercard", `<i class="fab fa-cc-mastercard"></i>`],
+  },
+  {
+    id: "#SK2541",
+    billingName: "Jamal Burnett",
+    date: "07 Oct, 2019",
+    total: "$380",
+    status: "Chargeback",
+    paymentMethod: ["Visa", `<i class="fab fa-cc-visa"></i>`],
+  },
+  {
+    id: "#SK2542",
+    billingName: "Juan Mitchell",
+    date: "06 Oct, 2019",
+    total: "$384",
+    status: "Paid",
+    paymentMethod: ["Paypal", `<i class="fab fa-cc-paypal"></i>`],
+  },
+  {
+    id: "#SK2543",
+    billingName: "Barry Dick",
+    date: "05 Oct, 2019",
+    total: "$412",
+    status: "Paid",
+    paymentMethod: ["Mastercard", `<i class="fab fa-cc-mastercard"></i>`],
+  },
+  {
+    id: "#SK2544",
+    billingName: "Ronald Taylor",
+    date: "04 Oct, 2019",
+    total: "$404",
+    status: "Refund",
+    paymentMethod: ["Visa", `<i class="fab fa-cc-visa"></i>`],
+  },
+  {
+    id: "#SK2545",
+    billingName: "Jacob Hunter",
+    date: "04 Oct, 2019",
+    total: "$392",
+    status: "Paid",
+    paymentMethod: ["Paypal", `<i class="fab fa-cc-paypal"></i>`],
+  },
+];
+
+let list = data.map((item) => {
+  return `<div class="transaction-list_item">
+  <div class="list-checkbox">
+    <input type="checkbox" />
+  </div>
+  <div class="list-id">${item.id}</div>
+  <div class="list-billing">${item.billingName}</div>
+  <div class="list-date">${item.date}</div>
+  <div class="list-total">${item.total}</div>
+  <div class="list-status ${item.status}">${item.status}</div>
+  <div class="list-method">${item.paymentMethod[1]}${item.paymentMethod[0]}</div>
+  <button class="list-button">View Details</button>
+</div>`;
+}).join("");
+
+transactionList.insertAdjacentHTML("beforeend", list);
